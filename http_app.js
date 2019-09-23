63c9eec5070db0e82b0e299665ed1ba1f7e8911d
Jasmine <ryeubi@gmail.com>
2019-09-23 18:03:08 +0900

    - optimizing - disarm container bug

--- http_app.js
+++ http_app.js
@@ -188,7 +188,7 @@ function retrieve_my_cnt_name(callback) {
             conf.cnt.push(JSON.parse(JSON.stringify(info)));
 
             my_parent_cnt_name = info.parent;
-            //my_cnt_name = my_parent_cnt_name + '/' + info.name;
+            my_cnt_name = my_parent_cnt_name + '/' + info.name;
             
             info.parent = '/Mobius/' + drone_info.gcs + '/Mission_Data';
             info.name = drone_info.drone;
@@ -368,7 +368,7 @@ function http_watchdog() {
         }
     }
     else if (sh_state === 'crtci') {
-        setTimeout(check_rtv_cnt, 10000);
+        //setTimeout(check_rtv_cnt, 10000);
     }
 }
 
