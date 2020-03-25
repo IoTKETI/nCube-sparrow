fcd464887c92790955b84955e998cc3627a452d1
IoTKETI <iot.keti@gmail.com>
2020-03-25 15:04:43 +0900

    Add files via upload

--- thyme_tas_mav.js
+++ thyme_tas_mav.js
@@ -370,7 +370,7 @@ exports.noti = function (path_arr, cinObj, socket) {
 exports.gcs_noti_handler = function (message) {
     if(my_drone_type === 'dji') {
         var com_msg = message.toString();
-        if (com_msg.substring(0,1) == ' ') {    
+        if (com_msg.substring(1,1) == ':') {    
             console.log(com_msg);
             socket_mav.write(message);
             // if (pre_com_msg == com_msg) {
