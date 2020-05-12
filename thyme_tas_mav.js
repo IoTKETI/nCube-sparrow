6944d108b911189c9f1b3d0a2157a5e172e34b86
IoTKETI <iot.keti@gmail.com>
2020-05-12 15:24:57 +0900

    Update thyme_tas_mav.js

--- thyme_tas_mav.js
+++ thyme_tas_mav.js
@@ -19,6 +19,11 @@ var moment = require('moment');
 
 var mavlink = require('./mavlibrary/mavlink.js');
 var fs = require('fs');
+
+// Button
+var Gpio = require('onoff').Gpio;
+var buttons = new Gpio(6, 'in', 'both');
+var exec = require('child_process').exec;
 /*
 // I2C
 var bus = 3;
@@ -875,6 +880,27 @@ function sendLteRssi(gpi) {
     });
 }
 
+buttons.watch(function (err, value) {
+  if (err){
+    console.error('There was an error', err);
+  return;
+  }
+  exec("sh gitpull.sh", function (error, stdout, stderr) {
+    console.log('stdout: ' + stdout);
+    console.log('stderr: ' + stderr);
+    if (error !== null) {
+      console.log('exec error: ' + error);
+    }
+  });
+  //console.log(value);
+});
+
+function unexportOnClose() {
+  button.unexport();
+  LED.writeSync(0);
+  LED.unexport();
+};
+
 // function displayMsg(msg) {
 // 	// oled.clearDisplay();
 //     oled.setCursor(1, 0);
@@ -883,3 +909,5 @@ function sendLteRssi(gpi) {
 //     sleep(1000);
 //     oled.clearDisplay();
 // }
+
+process.on('SIGINT', unexportOnClose);
