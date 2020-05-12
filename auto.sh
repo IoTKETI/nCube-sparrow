12508382a18c6124f9f18668308a64fdd41c076d
IoTKETI <iot.keti@gmail.com>
2020-05-12 07:14:51 +0100

    edit auto

--- auto.sh
+++ auto.sh
@@ -1,27 +1,8 @@
 #!/usr/bin/sh
 
-lte_flag=0
-ttyUSB=`ls /dev/ttyUSB3`
-while [ $lte_flag -eq 0 ]
-do
-  if [ -n "$ttyUSB" ]; then
-    echo "ttyUSB... OK ! "
-    if ping -q -c 1 -W 1 8.8.8.8 > /dev/null; then
-      lte_flag=1
-      echo "LTE... OK ! "
-    fi
-  else
-    echo "IPv4 is Down"
-    sleep 5
-    sudo reboot
-  fi
-done
-
-echo "IPv4 is Up"
 sudo chmod 777 /home/pi/nCube-sparrow/
 cd /home/pi/nCube-sparrow
 sudo chmod 777 *
-node sys_log.js
 git stash
 git pull
 sleep 5
