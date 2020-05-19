6a6e6253cae98b1804c1ef918508de4c2d0a0b28
IoTKETI <iot.keti@gmail.com>
2020-05-19 06:20:57 +0100

    delete print

--- sw_gitpull.py
+++ sw_gitpull.py
@@ -22,24 +22,24 @@ try:
     r_count = 0
     while True:
         button_val = GPIO.input(button_pin)
-        print('button_val: ', button_val)
+#        print('button_val: ', button_val)
         
         if(button_val == 0):
             if r_count > 350000:
                 flag = 1           
-                print("Ready")
+#                print("Ready")
             r_count += 1
         elif (button_val == 1 and flag == 1):
             count += 1
             if count > 100:
-                print("Start git pull")
+#                print("Start git pull")
                 # start git pull
                 os.system("./gitpull.sh")
                 flag = 0
             
         elif (button_val == 1):
             flag = 0
-            print("pass")
+#            print("pass")
             pass
             
     
