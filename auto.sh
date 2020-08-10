2fb4d86121654b2c8f5ed31b29caa0c8ed1c2f5e
dnjstjr93 <dnjstjr93@gmail.com>
2020-08-10 08:40:20 +0100

    edit autostart

--- auto.sh
+++ auto.sh
@@ -2,6 +2,9 @@
 
 sudo chmod 777 /home/pi/nCube-sparrow/
 cd /home/pi/nCube-sparrow
+git reset --hard HEAD
+git pull
+sleep 3
 sudo chmod 777 *
-python3 sw_gitpull.py&
+#python3 sw_gitpull.py&
 pm2 start thyme.js
